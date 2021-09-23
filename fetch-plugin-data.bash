49c9c1c8e14493ab81c134d3151fc39e07fc8b83
Michael Haynes <antihaynes@gmail.com>
2021-09-23 18:26:44 -0500

    Added 'docstrings', usage function, and altered flow
    Caller can now specify arbitrary function calls as the paramters to this script, as long as the function name is defined in it

--- fetch-plugin-data.bash
+++ fetch-plugin-data.bash
@@ -14,6 +14,7 @@ declare -A FIELDS=(
 declare -A URLS=(
     [base]="https://github.com"
     [api]="https://api.github.com/repos"
+    [raw]="https://raw.githubusercontent.com"
 )
 
 declare -A PROPS=(
@@ -21,11 +22,37 @@ declare -A PROPS=(
     [tags]=
     [config]=
     [comments]=
-    [timestamps]=
 )
 
+__get_docstring() {
+    eval "cat <<-EOF
+		`awk -v f="$1" '$1 == f {
+		        found = 1; next;
+		    }
+		    found == 1 {
+		        if ($1 == "#")
+		            print
+		        else 
+		            found = 0
+		    }' "${BASH_SOURCE[0]}" |
+		        sed 's/^[[:space:]]*# //'`
+		EOF"
+}
+
+_usage() {
+    # Display this message and exit
+    cat <<-USAGE
+		Usage: funcname \${urls[@]}
+		Functions: 
+		`while read -r line; do
+		    printf '  * %s\n' "${line#_}"
+		    __get_docstring "$line" | sed 's/^/    | /'
+		done < <(grep -o '^_[^_][^[:space:]]\+\(\)' "${BASH_SOURCE[0]}")`
+		USAGE
+}
 
 _api_query() {
+    # Get the raw JSON returned by the Github API for the passed URL
     local stem="${1#${URLS[base]}/}"
     local url="${URLS[api]}/${stem}"
     local -a opts=(
@@ -34,15 +61,35 @@ _api_query() {
         'Accept: application/vnd.github.preview'
     )
     curl "${opts[@]}" "$url"
+    return $?
 }
 
 _jq_query() {
+    # Get the properties stored in the array FIELDS from the JSON passed via stdin
+    # Fields: `printf '\n  - %s' "${FIELDS[@]}"`
     cat - | jq "{`for k in "${!FIELDS[@]}"; do \
         printf '"%s":%s,' "$k" "${FIELDS[$k]}"; done;`}"
+    return $?
+}
+
+_get_readme() {
+    # Get the README.md associated with the passed repository
+    # Print to stdout
+    local stem="${1#${URLS[base]}/}"
+    local url="${URLS[raw]}/${stem}/README.md"
+    curl "$url"
+    return $?
+}
+
+_get_plugin_data() {
+    # Pipes output of api_query to jq_query
+    _api_query "$1" | _jq_query
+    return $?
 }
 
 _get_props() {
-
+    # Not implemented
+    echo "Not implemented" >&2; exit 2
     for p in "${!PROPS[@]}"; do
         if [ -z "${PROPS[$p]}" ]; then
             PROPS[$p]="$(_user_query "$p")"
@@ -51,10 +98,12 @@ _get_props() {
 }
 
 main() {
-    while [ $# -gt 0 ]; do
-        _api_query "$1" | _jq_query
-        shift
-    done
+    if grep -q -s "^_${1}()" "${BASH_SOURCE[0]}"; then
+        local func="_${1}"; shift
+        $func "$@"; exit $?
+    else
+        _usage; exit -1
+    fi
 }
 
 main "$@"
