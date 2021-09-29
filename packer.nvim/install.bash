1e4025dda3a604a2a6c2d12d3da5eaaf3ddf54c2
Michael Haynes <antihaynes@gmail.com>
2021-09-28 20:50:25 -0500

    Made install script more generic

--- packer.nvim/install.bash
+++ packer.nvim/install.bash
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 export NVIM_PLUGIN_DIR="${NVIM_PLUGIN_DIR:-$HOME/.local/share/nvim/site/pack}"
+declare url="$(yq -r '.url' info.yaml)"
+declare name="$(yq -r '.name' info.yaml)"
+declare dest="${NVIM_PLUGIN_DIR}/${name%.*}/start/${name}"
+declare repo="`pwd`/${name}"
+declare -a GIT_CMD=(
+    git submodule add
+)
+declare -a GIT_OPTS=(
+    --depth 1
+)
 
 main() {
-    if mkdir -p "$NVIM_PLUGIN_DIR" || [ -d "$NVIM_PLUGIN_DIR" ]; then
-        local url="$(yq -r '.url' info.yaml)"
-        local name="$(yq -r '.name' info.yaml)"
-        local dest="${NVIM_PLUGIN_DIR}/${name%.*}/start/${name}"
-        local -a opts=( --depth 1 )
-        git clone "${opts[@]}" "$url" "$dest"
-    else
-        echo "Unable to locate ${NVIM_PLUGIN_DIR@A}" >&2
-        exit 1
-    fi
+    ${GIT_CMD[@]} ${GIT_OPTS[@]} -- "$url" "$repo" &&
+        if mkdir -p "$NVIM_PLUGIN_DIR" || [ -d "$NVIM_PLUGIN_DIR" ] 
+        then
+            ln -s "$repo" "$dest"
+        else
+            echo "Unable to locate ${NVIM_PLUGIN_DIR@A}" >&2
+            exit 1
+        fi
     exit $?
 }
 
