b49e5a8b2029b9bee0bb7d2adcaccdb49258582a
Michael Haynes <antihaynes@gmail.com>
2021-09-29 12:02:57 -0500

    Added function to add attrs to info.yaml files

--- get_urls.bash
+++ get_urls.bash
@@ -9,3 +9,15 @@ get_urls ()
     echo "Obtaining repository URLs from API endpoint '$url'..." >&2
     yq -i -S -Y '.urls=$urls[] | del(.url)'  "$fname" --slurpfile urls <(get_url_arr "$url")
 }
+
+add_attrs() {
+    local attr="$1" && shift
+    local -n vals="$1" && shift || return -1
+    for f in "$@"; do
+        yq -Y -S -i '
+    $ARGS.positional[0] as $n |
+    $ARGS.positional[1:] as $vs |
+    .[$n]+=($vs | map({($n):.,"timestamp":(now|todate)}))
+        ' "$f" --args "${vals[@]}"
+    done
+}
