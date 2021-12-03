768f0dd0ad59ac9f15722435766029a7b5eac497
Michael Haynes <antihaynes@gmail.com>
2021-12-03 13:18:17 -0600

    Add config for TS-playground

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,12 @@ if not present then
 end
 
 nvim_treesitter.setup {
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false -- Whether the query persists across vim sessions
+  },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = true,
