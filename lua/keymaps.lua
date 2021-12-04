dd99aca168df3ca649e3b1882693d535cb25a03b
Michael Haynes <antihaynes@gmail.com>
2021-12-03 21:26:07 -0600

    Add explanatory comment

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,6 +2,8 @@ local utils = require('utils')
 utils.map('n', '<C-l>', '<cmd>noh<CR>') -- Clear highlights
 utils.map('i', 'jl', '<Esc>')           -- jl to escape
 
+-- Telescope keybindings
+
 utils.map('n', '<leader>ff', '<cmd>Telescope find_files<CR>')
 utils.map('n', '<leader>fg', '<cmd>Telescope live_grep<CR>')
 utils.map('n', '<leader>fb', '<cmd>Telescope buffers<CR>')
