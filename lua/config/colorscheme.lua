c2d096cd1cb57108c8e9365fd49eaff3a894e13d
Michael Haynes <antihaynes@gmail.com>
2021-12-03 21:27:33 -0600

    Add some commonsense settings

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -2,3 +2,6 @@ local utils = require('utils')
 local cmd = vim.cmd
 utils.opt('o', 'termguicolors', true)
 cmd 'colorscheme gruvbox-material'
+vim.g.edge_style = 'aura'
+vim.g.edge_enable_italic = 1
+vim.g.disable_italic_comment = 1
