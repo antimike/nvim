e15388d224854c5c24c12d07b475e64212b6cf48
Michael Haynes <antihaynes@gmail.com>
2021-12-05 21:19:43 -0600

    Merge completion modules

--- lua/completion.lua
+++ lua/completion.lua
@@ -6,3 +6,6 @@ vim.g.completion_matching_strategy_list = {'exact', 'substring', 'fuzzy'}
 vim.g.diagnostic_enable_virtual_text = 1
 vim.g.completion_enable_snippet = 'UltiSnips'
 vim.g.completion_enable_auto_popup = 1
+-- <Tab> to navigate the completion menu
+utils.map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', {expr = true})
+utils.map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
