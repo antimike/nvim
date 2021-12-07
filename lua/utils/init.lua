3e5974880630c49d10aeea0d16ce71efcaa583f2
Michael Haynes <antihaynes@gmail.com>
2021-12-06 18:07:10 -0600

    Add fn for concatenating arrays

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -14,4 +14,13 @@ function utils.map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+function utils.concat(...)
+    local args = {...}
+    local ret = {}
+    for _,tab in ipairs(args) do
+        table.move(tab, 1, #tab, #ret+1, ret)
+    end
+    return ret
+end
+
 return utils
