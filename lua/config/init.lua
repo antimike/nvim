9e3c424281e884b109f6384dc55df061e8f34307
Michael Haynes <antihaynes@gmail.com>
2022-02-10 18:38:09 -0600

    Wrap requires with pcall

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,2 +1,7 @@
-require('config.sensible')
-return require('config.plugins')
+local loaded, plugins = pcall(require('config.plugins'))
+if loaded then
+    require('config.theme')
+    require('config.keymaps')
+    require('config.sensible')
+    return plugins
+end
