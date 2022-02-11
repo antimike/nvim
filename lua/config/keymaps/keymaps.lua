f663e3a24d5b3bb224960edab8462a2b118fc8a3
Michael Haynes <antihaynes@gmail.com>
2022-02-10 18:39:32 -0600

    Add ideas for how to structure keymaps

--- lua/config/keymaps/keymaps.lua
+++ lua/config/keymaps/keymaps.lua
@@ -3,11 +3,11 @@ local utils = require('utils')
 -- Map leader to space
 vim.g.mapleader = ' '
 
+-- Temp: replace with plugins
 utils.map('n', '<C-l>', '<cmd>noh<CR>') -- Clear highlights
 utils.map('i', 'jl', '<Esc>')           -- jl to escape
 
 -- Telescope keybindings
-
 utils.map('n', '<leader>ff', '<cmd>Telescope find_files<CR>')
 utils.map('n', '<leader>fg', '<cmd>Telescope live_grep<CR>')
 utils.map('n', '<leader>fb', '<cmd>Telescope buffers<CR>')
@@ -15,11 +15,67 @@ utils.map('n', '<leader>fh', '<cmd>Telescope help_tags<CR>')
 utils.map('n', '<leader>fl', '<cmd>Telescope git_files<CR>')
 
 -- DAP keybindings
--- TODO: Refactor into dedicated submodule
 
---require('telescope').load_extension('dap')
+require('telescope').load_extension('dap')
 --require('dbg.python')
 
+local keymap = {
+    n = {
+        ["<leader>dc"] = {'dap', 'continue'},
+        ["<leader>dsv"] = {'dap', 'step_over'},
+        ["<leader>dsi"] = {'dap', 'step_into'},
+        ["<leader>dso"] = {'dap', 'step_out'},
+        ["<leader>dtb"] = {'dap', 'toggle_breakpoint'},
+        ["<leader>dsbr"] = {'dap', 'set_breakpoint_conditional'},
+        ["<leader>dsbm"] = {'dap', 'set_breakpoint_logged'},
+        ["<leader>dro"] = {'dap', 'repl_open'},
+        ["<leader>drl"] = {'dap', 'repl_run_last'},
+        ["<leader>ff"] = {'search', 'files'},
+        ["<leader>fg"] = {'search', 'grep'},
+        ["<leader>fb"] = {'search', 'buffers'},
+        ["<leader>fh"] = {'search', 'help_tags'},
+        ["<leader>fl"] = {'search', 'git_files'},
+        ["<leader><C-l>"] = '<cmd>noh<CR>',
+        jl = '<Esc>',
+    },
+}
+
+-- telescope-dap
+utils.map('n', '<leader>dcc', '<cmd>lua require"telescope".extensions.dap.commands{}<CR>')
+utils.map('n', '<leader>dco', '<cmd>lua require"telescope".extensions.dap.configurations{}<CR>')
+utils.map('n', '<leader>dlb', '<cmd>lua require"telescope".extensions.dap.list_breakpoints{}<CR>')
+utils.map('n', '<leader>dv', '<cmd>lua require"telescope".extensions.dap.variables{}<CR>')
+utils.map('n', '<leader>df', '<cmd>lua require"telescope".extensions.dap.frames{}<CR>')
+
+local dap = {
+    continue = nil,
+    step_over = nil,
+    step_into = nil,
+    step_out = nil,
+    toggle_breakpoint = nil,
+    set_breakpoint = {
+        conditional = nil,
+        logged = nil
+    },
+    repl = {
+        open = nil,
+        run_last = nil
+    },
+    search = {
+        commands = nil,
+        configurations = nil,
+        list_breakpoints = nil,
+        variables = nil,
+        frames = nil
+    }
+}
+local search = {
+    files = nil,
+    grep = nil,
+    buffers = nil,
+    help_tags = nil,
+    git_files = nil,
+}
 utils.map('n', '<leader>dc', '<cmd>lua require"dap".continue()<CR>') 
 utils.map('n', '<leader>dsv', '<cmd>lua require"dap".step_over()<CR>') 
 utils.map('n', '<leader>dsi', '<cmd>lua require"dap".step_into()<CR>') 
@@ -30,9 +86,3 @@ utils.map('n', '<leader>dsbm', '<cmd>lua require"dap".set_breakpoint(nil, nil, v
 utils.map('n', '<leader>dro', '<cmd>lua require"dap".repl.open()<CR>') 
 utils.map('n', '<leader>drl', '<cmd>lua require"dap".repl.run_last()<CR>') 
 
--- telescope-dap
-utils.map('n', '<leader>dcc', '<cmd>lua require"telescope".extensions.dap.commands{}<CR>')
-utils.map('n', '<leader>dco', '<cmd>lua require"telescope".extensions.dap.configurations{}<CR>')
-utils.map('n', '<leader>dlb', '<cmd>lua require"telescope".extensions.dap.list_breakpoints{}<CR>')
-utils.map('n', '<leader>dv', '<cmd>lua require"telescope".extensions.dap.variables{}<CR>')
-utils.map('n', '<leader>df', '<cmd>lua require"telescope".extensions.dap.frames{}<CR>')
