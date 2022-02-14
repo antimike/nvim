1d6292c613254e6cace2a3b4f59bc0b746e565c1
Michael Haynes <antihaynes@gmail.com>
2022-02-14 11:28:32 -0600

    Use simpler Packer init code from README

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,49 +1,39 @@
-local use = require('packer').use
+-- local use = require('packer').use
 -- local user_plugins = require('../user_settings')
 
-return require('packer').startup({function()
-  local plugin_files = {
-    "effects",
-    "find",
-    "colors",
-    "text",
-    "workspace",
-    "focus",
-    "completion",
-    "lang",
-    "../user_settings"
-  }
-  use {'wbthomason/packer.nvim'}
---   for _, plugin in ipairs(plugins) do
---     if type(plugin) == "string" then
---       use { plugin }
---     elseif type(plugin) == "table" then
---       use(plugin)
---     end
---   end
-  for _, file in ipairs(plugin_files) do
-    local tbl = require(file)
-    if type(tbl) ~= "table" then
-      print("Unexpected return type " .. type(tbl) .. " from file " .. file)
-    end
-    for _, p in ipairs(tbl) do
-      -- I have no idea why this is necessary, but it is
-      if type(p) == "table" then
-        -- use{table.unpack(p)}
-        use(p)
-      elseif type(p) == "string" then
-        use{p}
-      end
-    end
+Plugins = {
+  {'wbthomason/packer.nvim', opt = true}
+}
+local plugin_files = {
+  "effects",
+  -- "find",
+  -- "colors",
+  -- "text",
+  -- "workspace",
+  -- "focus",
+  -- "completion",
+  -- "lang",
+  -- "../user_settings"
+}
+for _, f in ipairs(plugin_files) do
+  local ps = require(f)
+  if type(ps) == "table" then
+    vim.list_extend(Plugins, ps)
+  else
+    print("Unexpected return type " .. type(ps) .. " from file " .. f)
   end
-  -- Import settings of plugins or start plugins.
-  require('lsp_signature').setup()
-  require("which-key").setup()
-end,
-config = {
-  display = {
-    open_fn = function()
-      return require('packer.util').float({ border = 'single' })
-    end
+end
+
+return require('packer').startup({
+    -- Import settings of plugins or start plugins.
+    -- require('lsp_signature').setup()
+    -- require("which-key").setup()
+  Plugins,
+  config = {
+    display = {
+      open_fn = function()
+        return require('packer.util').float({ border = 'single' })
+      end
+    }
   }
-}})
+})
