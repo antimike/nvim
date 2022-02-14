214c4952987dfde52c22d573179e6bd0408b4382
Michael Haynes <antihaynes@gmail.com>
2022-02-14 10:30:49 -0600

    Resolve variable scoping issue

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,39 +1,44 @@
 local use = require('packer').use
 -- local user_plugins = require('../user_settings')
 
-local plugins = {'wbthomason/packer.nvim'}
-local function get_plugins(...)
-  local paths = table.pack(...)
-  for path in ipairs(paths) do
-    local found, tbl = pcall(require, path)
-    if found then
-      for p in ipairs(tbl) do
-        table.insert(plugins, p)
+return require('packer').startup({function()
+  local plugin_files = {
+    "effects",
+    "find",
+    "colors",
+    "text",
+    "workspace",
+    "focus",
+    "completion",
+    "lang",
+    "../user_settings"
+  }
+  use {'wbthomason/packer.nvim'}
+--   for _, plugin in ipairs(plugins) do
+--     if type(plugin) == "string" then
+--       use { plugin }
+--     elseif type(plugin) == "table" then
+--       use(plugin)
+--     end
+--   end
+  for _, file in ipairs(plugin_files) do
+    local tbl = require(file)
+    if type(tbl) ~= "table" then
+      print("Unexpected return type " .. type(tbl) .. " from file " .. file)
+    end
+    for _, p in ipairs(tbl) do
+      -- I have no idea why this is necessary, but it is
+      if type(p) == "table" then
+        -- use{table.unpack(p)}
+        use(p)
+      elseif type(p) == "string" then
+        use{p}
       end
     end
   end
-end
-
-get_plugins(
-  "colors"
-)
-
-return require('packer').startup({function()
-  use { 'wbthomason/packer.nvim' }
-
-
-  for _, plugin in ipairs(plugins) do
-    if type(plugin) == "string" then
-      use { plugin }
-    elseif type(plugin) == "table" then
-      use(plugin)
-    end
-  end
-
   -- Import settings of plugins or start plugins.
-  -- require('lsp_signature').setup()
-  -- require("which-key").setup()
-
+  require('lsp_signature').setup()
+  require("which-key").setup()
 end,
 config = {
   display = {
