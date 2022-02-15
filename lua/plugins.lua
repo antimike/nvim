d9f4d7a82a184cafbffad527078864cb9148cd4c
Michael Haynes <antihaynes@gmail.com>
2022-02-15 09:16:07 -0600

    Restrict plugins to just which-key and packer

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,34 +1,14 @@
--- local use = require('packer').use
--- local user_plugins = require('../user_settings')
-
-Plugins = {
-  {'wbthomason/packer.nvim', opt = true}
-}
-local plugin_files = {
-  "effects",
-  -- "find",
-  -- "colors",
-  -- "text",
-  -- "workspace",
-  -- "focus",
-  -- "completion",
-  -- "lang",
-  -- "../user_settings"
-}
-for _, f in ipairs(plugin_files) do
-  local ps = require(f)
-  if type(ps) == "table" then
-    vim.list_extend(Plugins, ps)
-  else
-    print("Unexpected return type " .. type(ps) .. " from file " .. f)
-  end
-end
-
 return require('packer').startup({
-    -- Import settings of plugins or start plugins.
     -- require('lsp_signature').setup()
-    -- require("which-key").setup()
-  Plugins,
+  {
+    {'wbthomason/packer.nvim', opt = true},
+    {
+      "folke/which-key.nvim",
+      config = function()
+        require("plugins.whichkey").setup()
+      end,
+    }
+  },
   config = {
     display = {
       open_fn = function()
