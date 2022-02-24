c9a28a7da20a1677563841419d560954f828d30e
Michael Haynes <antihaynes@gmail.com>
2022-02-24 13:24:00 -0600

    [BUG] Disable cheatsheet Telescope mappings
    TODO: Figure out what the problem is and restore

--- lua/config/cheatsheets.lua
+++ lua/config/cheatsheets.lua
@@ -21,10 +21,10 @@ require("cheatsheet").setup {
     include_only_installed_plugins = true,
 
     -- Key mappings bound inside the telescope window
-    telescope_mappings = {
-        ['<CR>'] = require('cheatsheet.telescope.actions').select_or_fill_commandline,
-        ['<A-CR>'] = require('cheatsheet.telescope.actions').select_or_execute,
-        ['<C-Y>'] = require('cheatsheet.telescope.actions').copy_cheat_value,
-        ['<C-E>'] = require('cheatsheet.telescope.actions').edit_user_cheatsheet,
-    }
+    -- telescope_mappings = {
+    --     ['<CR>'] = require('cheatsheet.telescope.actions').select_or_fill_commandline,
+    --     ['<A-CR>'] = require('cheatsheet.telescope.actions').select_or_execute,
+    --     ['<C-Y>'] = require('cheatsheet.telescope.actions').copy_cheat_value,
+    --     ['<C-E>'] = require('cheatsheet.telescope.actions').edit_user_cheatsheet,
+    -- }
 }
