4da25d63b54a541977e2166d70524ea2f6187b4c
Michael Haynes <antihaynes@gmail.com>
2022-02-28 15:44:19 -0600

    Add FTerm + IRON + cfgs

--- lua/plugins/integrations.lua
+++ lua/plugins/integrations.lua
@@ -1,121 +1,135 @@
 --- Plugins that integrate with external utilities (REPLs, tmux, etc.)
 return {
-  -- Terminal.
-  {
-    'akinsho/nvim-toggleterm.lua',
-    as = "toggleterm",
-    cmd = 'ToggleTerm',
-    setup = function()
-      require('config.toggleterm')
-    end
-  },
+	-- Terminal.
+	{
+		"akinsho/nvim-toggleterm.lua",
+		as = "toggleterm",
+		cmd = "ToggleTerm",
+		setup = function()
+			require("config.toggleterm")
+		end,
+	},
 
-  {
-    'pwntester/octo.nvim',
-    requires = {
-      'plenary',
-      'telescope',
-      'devicons',
-    },
-    config = function ()
-      require("config.octo")
-    end,
-  },
+	{
+		"pwntester/octo.nvim",
+		requires = {
+			"plenary",
+			"telescope",
+			"devicons",
+		},
+		config = function()
+			require("config.octo")
+		end,
+	},
 
-  -- Create shareable permalinks with line ranges
-  {
-    'ruifm/gitlinker.nvim',
-    as = "gitlinker",
-    requires = 'plenary',
-    config = function()
-      require("config.gitlinker").setup()
-    end
-  },
+	-- Create shareable permalinks with line ranges
+	{
+		"ruifm/gitlinker.nvim",
+		as = "gitlinker",
+		requires = "plenary",
+		config = function()
+			require("config.gitlinker").setup()
+		end,
+	},
 
-  -- Collaborative editing
-  {
-    'jbyuki/instant.nvim',
-    config = function()
-      vim.cmd('let g:instant_username = "antimike"')
-    end
-  },
+	-- Collaborative editing
+	{
+		"jbyuki/instant.nvim",
+		config = function()
+			vim.cmd('let g:instant_username = "antimike"')
+		end,
+	},
 
-  -- Competitive programming
-  {
-    'xeluxee/competitest.nvim',
-    as = 'competitest',
-    requires = 'nui',
-    config = function()
-        require('config.competitest')
-    end
-  },
+	-- Competitive programming
+	{
+		"xeluxee/competitest.nvim",
+		as = "competitest",
+		requires = "nui",
+		config = function()
+			require("config.competitest")
+		end,
+	},
 
-  -- {
-  --   'p00f/cphelper',
-  --   as = 'cphelper',
-  --   requires = 'plenary',
-  -- },
+	-- {
+	--   'p00f/cphelper',
+	--   as = 'cphelper',
+	--   requires = 'plenary',
+	-- },
 
-  -- Code runner
-  {
-    'michaelb/sniprun',
-    as = "sniprun",
-    run = 'bash ./install.sh',
-    config = function()
-        require('config.sniprun')
-    end
-  },
-  -- Interactive code scratchpad
-  {
-    'metakirby5/codi.vim',
-    as = "scratchpad",
-  },
+	-- Code runner
+	{
+		"michaelb/sniprun",
+		as = "sniprun",
+		run = "bash ./install.sh",
+		config = function()
+			require("config.sniprun")
+		end,
+	},
+	-- Interactive code scratchpad
+	{
+		"metakirby5/codi.vim",
+		as = "scratchpad",
+	},
 
-  -- Literate code support
-  {
-    'jbyuki/ntangle.nvim',
-    as = "ntangle",
-  },
+	-- Literate code support
+	{
+		"jbyuki/ntangle.nvim",
+		as = "ntangle",
+	},
 
-  -- Dash (script runner)
-  {
-    'jbyuki/dash.nvim',
-    as = "dash",
-  },
-  {
-    'Olical/conjure',
-    as = "conjure",
-  },
-  {
-    'HiPhish/repl.nvim',
-    as = "repl",
-    config = function()
-        require("config.repl")
-    end,
-  },
-  {
-    'bfredl/nvim-ipy',
-    as = "ipy",
-    config = function()
-        require("config.ipy")
-    end,
-  },
-  {
-    'is0n/fm-nvim',
-    as = "file-managers",
-    config = function()
-        require("config.file-managers")
-    end,
-  },
---   {
---     'sudormrfbin/cheatsheet.nvim',
---     requires = {
---         'telescope',
---         'popup',
---         'plenary',
---     },
---     config = function()
---         require('config.cheatsheets')
---     end,
--- }
+	-- Dash (script runner)
+	{
+		"jbyuki/dash.nvim",
+		as = "dash",
+	},
+	{
+		"Olical/conjure",
+		as = "conjure",
+	},
+	{
+		"HiPhish/repl.nvim",
+		as = "repl",
+		config = function()
+			require("config.repl")
+		end,
+	},
+	{
+		"bfredl/nvim-ipy",
+		as = "ipy",
+		config = function()
+			require("config.ipy")
+		end,
+	},
+	{
+		"is0n/fm-nvim",
+		as = "file-managers",
+		config = function()
+			require("config.file-managers")
+		end,
+	},
+	{
+		"https://github.com/hkupty/iron.nvim",
+		as = "iron",
+		config = function()
+			require("config.iron")
+		end,
+	},
+	{
+		"numToStr/FTerm.nvim",
+		as = "FTerm",
+		config = function()
+			require("config.fterm")
+		end,
+	},
+	--   {
+	--     'sudormrfbin/cheatsheet.nvim',
+	--     requires = {
+	--         'telescope',
+	--         'popup',
+	--         'plenary',
+	--     },
+	--     config = function()
+	--         require('config.cheatsheets')
+	--     end,
+	-- }
 }
