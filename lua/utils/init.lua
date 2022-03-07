0a2cc4c0c62ce4792cb5c66e2f42008b314d428e
Michael Haynes <antihaynes@gmail.com>
2022-03-06 22:46:43 -0600

    Comment out debug print statements

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -48,16 +48,16 @@ local function eulerian_coroutine(obj)
         repeat
                 if curr == nil then     -- Edge case: curr == nil  ==> we've exhausted the current level
                         -- NOTE: Will obviously cause problems if obj == nil.  This is a good thing
-                        print_debug_msg("Exhausted " .. tostring(curr) .. "; popping from stack...")
+                        -- print_debug_msg("Exhausted " .. tostring(curr) .. "; popping from stack...")
                         curr, key = table.remove(stack), table.remove(path)
                 end
                 -- Insert the node being searched and determine whether we can go deeper
-                print_debug_msg("Searching " .. tostring(curr) .. " for children...")
+                -- print_debug_msg("Searching " .. tostring(curr) .. " for children...")
                 table.insert(stack, curr)
                 descend, key, curr = pcall(next, curr, key)
 		if descend then
                         if key ~= nil then
-                                print_debug_msg("Found child with key " .. tostring(key) .. "!  Descending...")
+                                -- print_debug_msg("Found child with key " .. tostring(key) .. "!  Descending...")
                                 table.insert(path, key)
                                 -- If we're descending then we know we haven't seen this node before, since this is a DFS
                                 key = nil
@@ -68,7 +68,7 @@ local function eulerian_coroutine(obj)
                 else
                         -- Yield the full path to the leaf we just found
                         -- The last item on the stack will be non-table-like and the value of the leaf itself, so we pop it here
-                        print_debug_msg("Attempted to descend into leaf with value " .. tostring(stack[#stack]) .. "!  Yielding leaf...")
+                        -- print_debug_msg("Attempted to descend into leaf with value " .. tostring(stack[#stack]) .. "!  Yielding leaf...")
                         key = table.remove(path)
                         coroutine.yield(path, key, table.remove(stack))
                         curr = table.remove(stack)
