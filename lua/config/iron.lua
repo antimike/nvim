e4c2c25146ebe626964cdee3fa618b0c6f738a76
Michael Haynes <antihaynes@gmail.com>
2022-05-23 14:12:17 -0500

    Remove deprecated config opts

--- lua/config/iron.lua
+++ lua/config/iron.lua
@@ -1,18 +1,5 @@
 local iron = require("iron")
 
-iron.core.add_repl_definitions({
-	python = {
-		ipython = {
-			command = { "ipython" },
-		},
-	},
-	clojure = {
-		lein_connect = {
-			command = { "lein", "repl", ":connect" },
-		},
-	},
-})
-
 iron.core.set_config({
 	preferred = {
 		python = "ipython",
