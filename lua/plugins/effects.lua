0af9655636f523e5e012e5a27d43ee54b40cd004
Michael Haynes <antihaynes@gmail.com>
2022-05-23 11:47:11 -0500

    Lazy-load UI plugins

--- lua/plugins/effects.lua
+++ lua/plugins/effects.lua
@@ -1,64 +1,85 @@
 return {
-	-- Allows fancy UI hooks
-	-- Requires nvim 0.6 for full effect
-	-- NOTE: May conflict with popui and telescope-ui-select.nvim
-	{
-		"stevearc/dressing.nvim",
-		as = "dressing",
-		config = function()
-			require("config.dressing")
-		end,
-	},
+ -- Allows fancy UI hooks
+ -- Requires nvim 0.6 for full effect
+ -- NOTE: May conflict with popui and telescope-ui-select.nvim
+ {
+  "stevearc/dressing.nvim",
+  as = "dressing",
+  event = "UIEnter",
+  config = function()
+      require("config.dressing")
+  end,
+ },
 
-	-- Markdown Preview
-	{ "ellisonleao/glow.nvim" },
+ -- Markdown Preview
+ { "ellisonleao/glow.nvim", ft = "markdown" },
+ -- Register peeking
+ {
+   "gennaro-tedesco/nvim-peekup",
+   keys = '<leader>"',
+   config = function()
+       vim.cmd [[let g:peekup_open = '<leader>"']]
+   end
+ },
 
-	-- Register peeking
-	"gennaro-tedesco/nvim-peekup",
+ -- Auto-resizes splits
+ {
+  "beauwilliams/focus.nvim",
+  module = "focus",
+  cmd = { "FocusSplitNicely", "FocusSplitCycle" },
+  config = function()
+      require("config.focus")
+  end,
+ },
 
-	-- Auto-resizes splits
-	{
-		"beauwilliams/focus.nvim",
-		config = function()
-			require("config.focus")
-		end,
-	},
+ -- Popui: custom popups
+ -- NOTE: May conflict with dressing.nvim and telescope-ui-select.nvim
+ {
+  "RishabhRD/popfix",
+  as = "popfix",
+  module = "popfix"
+ },
 
-	-- Popui: custom popups
-	-- NOTE: May conflict with dressing.nvim and telescope-ui-select.nvim
-	{
-		"RishabhRD/popfix",
-	},
-
-	{
-		"hood/popui.nvim",
-		config = function()
-			vim.ui.select = require("popui.ui-overrider")
-		end,
-	},
-	{
-		"xiyaowong/nvim-transparent",
-		as = "transparent",
-		config = function()
-			require("config.transparent")
-		end,
-	},
-	{
-		"wfxr/minimap.vim",
-		as = "minimap",
-		config = function()
-			require("config.minimap")
-		end,
-	},
-	-- Quickfix popup
-	{
-		"kevinhwang91/nvim-bqf",
-                as = "bqf",
-                ft = "qf",
-		requires = "treesitter",
-		after = "treesitter",
-                config = function()
-                        require("config.bqf")
-                end,
-	},
+ {
+  "hood/popui.nvim",
+  requires = "popfix",
+  module = "popui",
+  config = function()
+      vim.ui.select = require("popui.ui-overrider")
+      vim.ui.input = require("popui.input-overrider")
+  end,
+ },
+ {
+  "xiyaowong/nvim-transparent",
+  as = "transparent",
+  config = function()
+      require("config.transparent")
+  end,
+ },
+ {
+  "wfxr/minimap.vim",
+  as = "minimap",
+  cmd = {
+          "Minimap",
+          "MinimapClose",
+          "MinimapToggle",
+          "MinimapRefresh",
+          "MinimapUpdateHighlight",
+          "MinimapRescan"
+  },
+  config = function()
+      require("config.minimap")
+  end,
+ },
+ -- Quickfix popup
+ {
+  "kevinhwang91/nvim-bqf",
+  as = "bqf",
+  ft = "qf",
+  requires = "treesitter",
+  after = "treesitter",
+  config = function()
+      require("config.bqf")
+  end,
+ },
 }
